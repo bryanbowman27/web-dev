d7008812cd6dae65ce25333fe019608a4afbc5ce
Bryan Bowman <Bryan@Bryan.local>
2017-03-28 13:38:39 -0700

    finished up code player project

--- 4-code-player/js/script.js
+++ 4-code-player/js/script.js
@@ -1,5 +1,80 @@
-$(function() {
-
-    $( "#selectable" ).selectable();
+function resize(){
+    var columns = 4;
+    var pageWidth = $('#codeContainer').width() - 10;
     
+    if ($('a#htmlButton').hasClass("selected") == false){
+        $('#htmlCodeContainer').hide();
+        columns -= 1;
+    }else{
+        $('#htmlCodeContainer').show();
+    }
+    
+    if ($('a#cssButton').hasClass("selected") == false){
+        $('#cssCodeContainer').hide();
+        columns -= 1;
+    }else{
+        $('#cssCodeContainer').show();
+    }
+    
+    if ($('a#jsButton').hasClass("selected") == false){
+        $('#jsCodeContainer').hide();
+        columns -= 1;
+    }else{
+        $('#jsCodeContainer').show();
+    }
+    
+    if ($('a#outputButton').hasClass("selected") == false){
+        $('#outputContainer').hide();
+        columns -= 1;
+    }else{
+        $('#outputContainer').show();
+    }
+         
+    $('#htmlCodeContainer').width(pageWidth / columns);
+    $('#cssCodeContainer').width(pageWidth / columns);
+    $('#jsCodeContainer').width(pageWidth / columns);
+    $('#outputContainer').width(pageWidth / columns);
+    
+}
+
+function buttonToggle(){
+
+    $('a#htmlButton').click(function(){
+        $(this).toggleClass("selected");
+        resize();
+    });
+    $('a#cssButton').click(function(){
+        $(this).toggleClass("selected");
+        resize();
+    });
+    $('a#jsButton').click(function(){
+        $(this).toggleClass("selected");
+        resize();
+    });
+    $('a#outputButton').click(function(){
+        $(this).toggleClass("selected");
+        resize();
+    });   
+}
+
+function updateOutput(){
+    $("#outputContainer").contents().find("html").html("<html><head><style type='text/css'>" + $('#cssCode').val() + "</style></head><body>" + $('#htmlCode').val() + "</body></html>");
+    
+    document.getElementById("outputContainer").contentWindow.eval($('#jsCode').val());
+    
+}
+
+$("textarea").on('change keyup paste', function(){
+    
+    updateOutput();
+    
+});
+    
+$(document).ready(function(){
+    
+    buttonToggle();   
+    
+    resize();  
+    
+    updateOutput();
 });
